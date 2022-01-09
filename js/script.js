763669edb30afb1eb67d0b98a785647626146327
Mogboluwaga Onayade <mogbo18@yahoo.com>
2022-01-09 15:44:23 +0100

    Update on the Collections tab, added Animations

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ document.querySelector('#bar').onclick = () =>{
 window.onscroll = () => {
     navbar.classList.remove('active')
 }
+
+//filter
+let filterBtn = document.querySelectorAll('.filter-buttons .filter');
+let filterItem = document.querySelectorAll('.collections .box-card .collect');
+
+filterBtn.forEach(button => {
+    button.onclick =()=>{
+        filterBtn.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        let dataFilter = button.getAttribute('data-filter');
+
+        filterItem.forEach(item => {
+
+            item.classList.remove('active');
+            item.classList.add('hide');
+
+            if (item.getAttribute('data-item') == dataFilter || dataFilter == 'all') {
+                item.classList.remove('hide');
+                item.classList.add('active');
+            }
+        })
+    }
+})
