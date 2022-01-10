693f85b6dde5594f205749ca76bdc0f4d21e45bf
Mogboluwaga Onayade <mogbo18@yahoo.com>
2022-01-10 12:09:10 +0100

    Added Collections Slider

--- js/script.js
+++ js/script.js
@@ -30,3 +30,23 @@ filterBtn.forEach(button => {
         })
     }
 })
+
+//collections
+var swiper = new Swiper(".card-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+  });
